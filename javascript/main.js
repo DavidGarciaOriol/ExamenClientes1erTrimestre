38d21c8571c73d24f5483f5b3d0865af5a9e2a83
David García Oriol <davidgarciaoriol@gmail.com>
2018-12-17 20:54:21 +0100

    A suspender =)

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
 
+    var errArr = [];
+
     var appArr = [];
 
     $("#appName").keyup(function(event) { 
         let appName = $("#appName").val();
-        let nameExpresion = /^[A-Z]+$/g;
+        let nameExpresion = /^[A-Z]$/g;
         let errAppName = $("<p> El nombre de la aplicación solo admite letras mayúsculas. </p>");
 
         if(!nameExpresion.test(appName.value)){
-            $("#appName").removeClass('validImput');
+            $("#appName").removeClass('validInput');
             $("#appName").addClass('errorInput');
             $(".formDiv").append(errAppName);
-            
+
+            errArr.push(errAppName);
         } else{
-            $("#appName").removeClass('errorImput');
-            $("#appName").addClass('validImput');
+            $("#appName").removeClass('errorInput');
+            $("#appName").addClass('validInput');
             $(errAppName).remove();
         }  
     });
@@ -23,79 +26,79 @@ $(document).ready(function(){
 
     $("#appLang").keyup(function(event) { 
         let appLang = $("#appLang").val();
-        let idExpresion = /^[0-9A-Za-z#]+$/g;
+        let idExpresion = /^[0-9A-Za-z#]$/g;
         let errAppLang = $("<p> El lenguaje del proyecto permite solo letras, números y el símbolo #.</p>");
 
         if(!idExpresion.test(appLang.value)){
-            $("#appLang").removeClass('validImput');
+            $("#appLang").removeClass('validInput');
             $("#appLang").addClass('errorInput');
-            $(".formDiv").append(errAppLang);
+            $(".formDiv").append(errApplang); 
         } else{
-            $("#appLang").removeClass('errorImput');
-            $("#appLang").addClass('validImput');
+            $("#appLang").removeClass('errorInput');
+            $("#appLang").addClass('validInput');
             $(errAppLang).remove();
         }
     }); 
 
-    $("#appTime").keyup(function(event) { 
+    $("#appTime").keyup(function(event) {
         let appTime = $("#appTime").val();
-        let errAppTime = $("<p> El tiempo no debe ser menor a 200 horas ni mayor a 1000 horas. </p>");
-
-        if(!(parseInt(appTime) >= 200 && parseInt(appTime) <= 1000)){
-            $("#appTime").removeClass('validImput');
+        var errAppTime = $("<p> El tiempo no debe ser menor a 200 horas ni mayor a 1000 horas, tampoco puede ser decmal. </p>");
+        
+        if(!(parseInt(appTime) >= 200 && parseInt(appTime) <= 1000 && appTime == parseInt(appTime))){
+            $("#appTime").removeClass('validInput');
             $("#appTime").addClass('errorInput');
-            $(".formDiv").append(errAppTime);
+            $(".formDiv").append(errAppTime); 
         } else{
-            $("#appTime").removeClass('errorImput');
-            $("#appTime").addClass('validImput');
+            $("#appTime").removeClass('errorInput');
+            $("#appTime").addClass('validInput');
             $(errAppTime).remove();
         }
     }); 
 
     $("#appAutor").keyup(function(event) { 
         let appAutor = $("#appAutor").val();
-        let idExpresion = /^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$/g;
+        let idExpresion = /^([A-Z]{1}[a-z]{1,}[\s]){2}([A-Z]{1}[a-z]{1,})$/g;
         let errAppAutor = $("<p> El campo permite Nombres y Apellidos, empezando por mayúsculas cada uno, y con espacio entre ellos.</p>");
 
         if(!idExpresion.test(appAutor.value)){
-            $("#appAutor").removeClass('validImput');
+            $("#appAutor").removeClass('validInput');
             $("#appAutor").addClass('errorInput');
             $(".formDiv").append(errAppAutor);
         } else{
-            $("#appAutor").removeClass('errorImput');
-            $("#appAutor").addClass('validImput');
+            $("#appAutor").removeClass('errorInput');
+            $("#appAutor").addClass('validInput');
             $(errAppAutor).remove();
         }
     });
 
     $("#appDni").keyup(function(event) { 
         let appDni = $("#appDni").val();
-        let idExpresion = /^[0-9]{8}[A-Z]{1}+$/g;
+        let idExpresion = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
         let errAppDni = $("<p> El campo DNI debe contener 8 números seguidos de una letra mayúscula. </p>");
 
-        if(!idExpresion.keyup(appDni.value)){
-            $("#appDni").removeClass('validImput');
+        if(!idExpresion.test(appDni.value)){
+            $("#appDni").removeClass('validInput');
             $("#appDni").addClass('errorInput');
             $(".formDiv").append(errAppDni);
         } else{
-            $("#appDni").removeClass('errorImput');
-            $("#appDni").addClass('validImput');
+            $("#appDni").removeClass('errorInput');
+            $("#appDni").addClass('validInput');
             $(errAppDni).remove();
         }
     }); 
 
     $("#appId").keyup(function(event) { 
         let appId = $("#appId").val();
-        let idExpresion = /^[0-9A-Fa-f]{6}[-]{1}[0-9]{1}[A-Z]{1}+$/g;
+        let idExpresion = /[0-9a-fA-F]{6}[-]{1}[0-9]{1}[A-Z]{1}/i;
         let errAppId = $("<p> El campo debe contener un código hexadecimal, seguido de un guión, el primernúmero de su DNI y la letra de dicho DNI. </p>");
 
         if(!idExpresion.test(appId.value)){
-            $("#appId").removeClass('validImput');
+            $("#appId").removeClass('validInput');
             $("#appId").addClass('errorInput');
-            $("#.formDiv").append(errAppId);
+            $(".formDiv").append(errAppId);
         } else{
-            $("#appId").removeClass('errorImput');
-            $("#appId").addClass('validImput');
+            $("#appId").removeClass('errorInput');
+            $("#appId").addClass('validInput');
             $(errAppId).remove();
         }
     }); 
@@ -103,8 +106,11 @@ $(document).ready(function(){
 
 
     $("#submit").click(function(event) { 
+
         event.preventDefault();
+
         var webApp = new AplicacionesWeb(appName, appTheme, appLang, appTime, appAutor, appDni, appId);
         appArr.push(webApp);
+        
     });
 });
