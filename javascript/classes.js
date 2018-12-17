0aa0dc097553481435f73616a5ee1e3bf5692bbe
David García Oriol <davidgarciaoriol@gmail.com>
2018-12-17 18:47:58 +0100

    class

--- javascript/classes.js
+++ javascript/classes.js
@@ -12,4 +12,59 @@ class AplicacionesWeb{
 
     }
 
+    get nombre(){
+        return this._nombre;
+    }
+
+    get tema(){
+        return this._tema;
+    }
+
+    get lenguaje(){
+        return this._lenguaje;
+    }
+
+    get tiempo(){
+        return this._tiempo;
+    }
+
+    get autor(){
+        return this._autor;
+    }
+
+    get dni(){
+        return this._dni;
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    set tema(tema){
+        this._tema = tema;
+    }
+
+    set lenguaje(lenguaje){
+        this._lenguaje = lenguaje;
+    }
+
+    set tiempo(tiempo){
+        this._tiempo = tiempo;
+    }
+
+    set autor(autor){
+        this._autor = autor;
+    }
+
+    set dni(dni){
+        this._dni = dni;
+    }
+
+    set id(id){
+        this._id = id;
+    }
 }
